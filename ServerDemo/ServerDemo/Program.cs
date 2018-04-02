a1e5cf95d0dbbc2c76e10a9d62c472f97f3e37f3
JamesHuntCode <jameshuntwork1997@yahoo.com>
2018-04-02 20:07:02 +0100

    server connection successful

--- ServerDemo/ServerDemo/Program.cs
+++ ServerDemo/ServerDemo/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerDemo
 {
@@ -10,7 +12,23 @@ namespace ServerDemo
     {
         static void Main(string[] args)
         {
+            // create socket listener
+            IPAddress IP = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(IP, 8080);
+            TcpClient client = default(TcpClient);
 
+            // connect to server
+            try
+            {
+                server.Start();
+                Console.WriteLine("Connection successfull!");
+                Console.Read();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err.Message);
+                Console.Read();
+            }
         }
     }
 }
